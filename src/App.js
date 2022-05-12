b64ca3c4e4c2829406dc9d694a338b1e9686700e
Andrew Smith <meuzishun@gmail.com>
2022-05-12 16:44:33 -0400

    Part 3

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
-import React from 'react';
-import Overview from './components/Overview';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      tasks: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      value: e.target.value,
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState({
-      value: '',
-      tasks: [...this.state.tasks, this.state.value],
-    });
-  }
-
+class App extends Component {
   render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <input type='submit' />
-        </form>
-        <Overview tasks={this.state.tasks} />
-      </div>
-    );
+    return <div>Hello World</div>;
   }
 }
 
