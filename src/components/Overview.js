e91d9a0ae9612714e6a2e7c7dc4f7a5d593eae35
Andrew Smith <meuzishun@gmail.com>
2022-05-13 14:09:24 -0400

    Change Overview to a class component and add a handleEditClick callback

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Overview = (props) => {
-  const { tasks, onTaskRemoval } = props;
+class Overview extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <ul>
-      {tasks.map((task, index) => {
-        return (
-          <li key={task.id} id={task.id}>
-            {index + 1}: {task.text} <button>edit</button>
-            <button onClick={onTaskRemoval}>remove</button>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick = (e) => {
+    console.log(e);
+  };
+
+  render() {
+    const { tasks, onTaskRemoval } = this.props;
+
+    return (
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li key={task.id} id={task.id}>
+              {index + 1}: {task.text}{' '}
+              <button onClick={this.handleEditClick}>edit</button>
+              <button onClick={onTaskRemoval}>remove</button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
 
 export default Overview;
